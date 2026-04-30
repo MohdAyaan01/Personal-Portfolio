a6e53eb1a054e3c0386900396dc9ba7465b7bda7
Mohd Ayaan Siddiqui <zxayaan2.0@gmail.com>
2026-04-30 20:07:05 +0530

    Initilal Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ExternalLink } from 'lucide-react';
 import Link from 'next/link';
-import Instagram from 'next-auth/providers/instagram';
+
 
 const GithubIcon = ({ className }: { className?: string }) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={className}>
